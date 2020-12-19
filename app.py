25785668dd7d85b3018193d9f4a2acd980650f68
Jaroslaw Kaszczak <kaszczak.jaroslaw@outlook.com>
2020-12-19 14:30:51 +0100

    Implement login

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import bcrypt
 
 import data
-from flask import Flask, session, url_for, render_template, request
-
-# put your code here
+from flask import Flask, session, url_for, render_template, request, redirect
 
 app = Flask(__name__)
 app.secret_key = bcrypt.gensalt()
 
 
 LOGIN_EMAIL = 'kaszczak.jaroslaw@outlook.com'
-PASSWORD = 'qwerty'
+HASH_PW = b'$2b$12$s8Qmmgn4m6eDXuq1KTdgI.Y2yF6kfoXKBl9xk0C6Bj7Z7FxSTkEsG'
 
 
 @app.route('/')
 def index():
     session['first_time'] = 'tak'
-    return render_template('index.html')
+    if 'user_name' in session:
+        user_name = session['user_name']
+    return render_template('index.html', user_name=user_name)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -25,10 +25,15 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
-        print(email)
-        print(password)
+        if email == LOGIN_EMAIL:
+            if bcrypt.checkpw(password.encode('UTF-8'), HASH_PW):
+                session['user_name'] = LOGIN_EMAIL
+                return redirect(url_for('index'))
+            else:
+                return render_template('login_form.html', bad_login=True)
 
-    return render_template('login_form.html')
+    elif request.method == 'GET':
+        return render_template('login_form.html')
 
 
 if __name__ == '__app__':
